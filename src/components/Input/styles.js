a9a9884b400ef3c81662aa0b8e25f3a2674fe760
Murilo Terra <murilo.18@outlook.com.br>
2024-02-06 20:47:42 -0400

    styles-updates

--- src/components/Input/styles.js
+++ src/components/Input/styles.js
@@ -15,7 +15,7 @@ export const InputContainer = styled.div`
   input {
     width: 100%;
     height: 75px;
-    background-color: #aaaaff;
+    background-color: #09968b;
     border: 0;
     float: right;
     font-size: 24px;
--- src/styles.js
+++ src/styles.js
@@ -4,7 +4,6 @@ export const Container = styled.div`
   width: 100%;
   height: 100vh;
   background-color: #cacaca;
-
   display: flex;
   align-items: center;
   justify-content: center;
